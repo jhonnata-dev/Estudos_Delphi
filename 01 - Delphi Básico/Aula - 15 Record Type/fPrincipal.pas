e0d8468bec9d552a9ae7f393debfaf77cf410dc6
jhonnata-dev <jhonnatadmartins@gmail.com>
2025-01-23 00:19:35 -0300

    Aula16 Record Type

---
+++ 01 - Delphi Básico/Aula - 15 Record Type/fPrincipal.pas
@@ -0,0 +1,80 @@
+unit fPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls;
+
+type RecordEndereco = Record
+  Rua: String;
+  Cidade: String;
+End;
+
+type RecordPessoa = Record
+  Codigo: Integer;
+  Nome: String;
+  Credito: Currency;
+  Ativo: Boolean;
+  Endereco: RecordEndereco;
+End;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    btnPessoa: TButton;
+    mmoPessoa: TMemo;
+    procedure btnPessoaClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btnPessoaClick(Sender: TObject);
+var
+  lPessoa: RecordPessoa;
+  lSituacao: String;
+begin
+
+  lPessoa.Codigo := 1;
+  lPessoa.Nome := 'Jhon';
+  lPessoa.Credito := 500;
+  lPessoa.Ativo := True;
+
+  lPessoa.Endereco.Rua := 'Rua das laranjeiras';
+  lPessoa.Endereco.Cidade := 'Bahia';
+
+  mmoPessoa.Lines.Clear;
+  mmoPessoa.Lines.Add('Dados da pessoa');
+  mmoPessoa.Lines.Add('---------------');
+  mmoPessoa.Lines.Add('Código: ' + lPessoa.Codigo.ToString);
+  mmoPessoa.Lines.Add('Nome: ' + lPessoa.Nome);
+
+  mmoPessoa.Lines.Add('Crédito: ' + FormatCurr('#,##0.00', lPessoa.Credito));
+
+  if lPessoa.Ativo then
+  begin
+    lSituacao := 'Ativo';
+  end
+  else
+  begin
+    lSituacao := 'Inativo';
+  end;
+
+  mmoPessoa.Lines.Add('Situação: ' + lSituacao);
+  mmoPessoa.Lines.Add('Rua: ' + lPessoa.Endereco.Rua);
+  mmoPessoa.Lines.Add('Cidade: ' + lPessoa.Endereco.Cidade);
+
+end;
+
+end.
