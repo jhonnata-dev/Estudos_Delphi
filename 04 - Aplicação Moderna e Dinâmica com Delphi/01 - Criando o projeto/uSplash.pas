57f219935b9f3aa050b6b65dd24f4136ce747ed6
Jhonnata-dev <jhonnatadmartins@gmail.com>
2025-02-17 17:03:55 -0300

    Criando o projeto

---
+++ 04 - Aplicação Moderna e Dinâmica com Delphi/01 - Criando o projeto/uSplash.pas
@@ -0,0 +1,26 @@
+unit uSplash;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons,
+  Vcl.Imaging.pngimage, Vcl.ExtCtrls;
+
+type
+  TfrmSplash = class(TForm)
+    Image1: TImage;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmSplash: TfrmSplash;
+
+implementation
+
+{$R *.dfm}
+
+end.
